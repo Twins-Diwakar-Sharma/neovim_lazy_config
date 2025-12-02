b732173f9587036f849e15dbb6a35f039e9ef4a7
Twins-Diwakar-Sharma <twins-diwakar-sharma@github.com>
2025-12-02 16:55:19 +0530

    added telescope

--- lua/GodEmperor/plugins/lspconfig.lua
+++ lua/GodEmperor/plugins/lspconfig.lua
@@ -8,12 +8,21 @@ return {
 
       local lspconfig = require('lspconfig')
       local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-      
+
+--[[
       -- ccls
       lspconfig.ccls.setup({
         capabilities = lsp_capabilities,
       })
       vim.keymap.set('n', '<Leader>hs', ':CclsSwitchSourceHeader<CR>')
+]]
+--[[
+    --clangd
+    lspconfig.clangd.setup({
+      capabilities = lsp_capabilities,
+      cmd = {'C:\\Development\\cclsCMAKE\\llvm-project\\Release\\bin\\clangd.exe'}
+    })
+    ]]
 
   end,
 }
