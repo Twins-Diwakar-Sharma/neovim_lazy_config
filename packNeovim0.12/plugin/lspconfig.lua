8368b9d420d0c7c9435780574fed2534112f0480
Diwakar Sharma <Twins-Diwakar-Sharma@github.com>
2026-06-24 15:40:57 +0530

    keymaps for lsp added

--- packNeovim0.12/plugin/lspconfig.lua
+++ packNeovim0.12/plugin/lspconfig.lua
@@ -12,3 +12,19 @@ vim.lsp.enable({
   'ccls',
 })
 
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(event)
+    local opts = {buffer = event.buf}
+    local teleBuiltin = require("telescope.builtin")
+
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts, {desc = "Goto Description"})
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts, {desc = "Goto Declaration"})
+    vim.keymap.set("n", "gr", teleBuiltin.lsp_references, opts, {desc = "Goto References"})
+    vim.keymap.set("n", "gi", teleBuiltin.lsp_implementations, opts, {desc = "Goto Implementation"})
+    vim.keymap.set("n", "gt", teleBuiltin.lsp_type_definitions, opts, {desc = "Goto type definitions"})
+
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts, {desc = "Hover Documentation"})
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts, {desc = "Hover Documentation"})
+  end,
+})
